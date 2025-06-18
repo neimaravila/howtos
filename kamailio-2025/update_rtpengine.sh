4398f936dd08e86fd0ac448e364926555f872438
Neimar Avila <neimar.avila@gmail.com>
2025-06-18 14:56:02 -0300

    Update update_rtpengine.sh

--- kamailio-2025/update_rtpengine.sh
+++ kamailio-2025/update_rtpengine.sh
@@ -30,3 +30,26 @@ else
   sed -i "s|^#interface = priv/IP_INTERNO;pub/IP_INTERNO!IP_EXTERNO$|interface = priv/$IP_INTERNO;pub/$IP_INTERNO!$IP_EXTERNO|" /etc/rtpengine/rtpengine.conf
   echo "Arquivo /etc/rtpengine/rtpengine.conf atualizado: linha 'interface = priv/$IP_INTERNO;pub/$IP_INTERNO!$IP_EXTERNO' descomentada e atualizada."
 fi
+
+# Verifica se o IP_INTERNO está associado a alguma interface de rede
+if ip addr show | grep -q "$IP_INTERNO"; then
+  echo "IP_INTERNO ($IP_INTERNO) encontrado em uma interface de rede."
+
+  # Substitui as linhas listen com IP_INTERNO
+  sed -i "s|^listen-ng = IPINTERNO:2223$|listen-ng = $IP_INTERNO:2223|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-tcp = IPINTERNO:2230$|listen-tcp = $IP_INTERNO:2230|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-udp = IPINTERNO:12222$|listen-udp = $IP_INTERNO:12222|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-http = IPINTERNO:2225$|listen-http = $IP_INTERNO:2225|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-cli = IPINTERNO:2231$|listen-cli = $IP_INTERNO:2231|" /etc/rtpengine/rtpengine.conf
+  echo "Arquivo /etc/rtpengine/rtpengine.conf atualizado: linhas 'listen-ng', 'listen-tcp', 'listen-udp', 'listen-http' e 'listen-cli' configuradas com IP_INTERNO ($IP_INTERNO)."
+else
+  echo "IP_INTERNO ($IP_INTERNO) não está associado a nenhuma interface de rede."
+
+  # Substitui IP_INTERNO por IP_EXTERNO em todas as linhas listen
+  sed -i "s|^listen-ng = IPINTERNO:2223$|listen-ng = $IP_EXTERNO:2223|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-tcp = IPINTERNO:2230$|listen-tcp = $IP_EXTERNO:2230|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-udp = IPINTERNO:12222$|listen-udp = $IP_EXTERNO:12222|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-http = IPINTERNO:2225$|listen-http = $IP_EXTERNO:2225|" /etc/rtpengine/rtpengine.conf
+  sed -i "s|^listen-cli = IPINTERNO:2231$|listen-cli = $IP_EXTERNO:2231|" /etc/rtpengine/rtpengine.conf
+  echo "Arquivo /etc/rtpengine/rtpengine.conf atualizado: linhas 'listen-ng', 'listen-tcp', 'listen-udp', 'listen-http' e 'listen-cli' configuradas com IP_EXTERNO ($IP_EXTERNO)."
+fi
